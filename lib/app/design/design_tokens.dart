769ffd735b1438c148e86f55eda016698e37db91
Marosvári Ádám <adammaro77@gmail.com>
2025-10-30 19:59:08 +0100

    first

---
+++ lib/app/design/design_tokens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DT{
+  static const s1=4.0, s2=8.0, s3=12.0, s4=16.0, s5=20.0, s6=24.0, s8=32.0;
+
+  static const bg = Color(0xFF25283D);
+  static const text = Color(0xFFEFD9CE);
+  static const cardGreen = Color(0xFF07BEB8);
+  static const cardRed = Color(0xFF8F3985);
+  static const pillBlue = Color(0xFF98DFEA);
+  static const pillOrange = Color(0xFFA85E38);
+}
